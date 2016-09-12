ba9197bc7b6bf1c55434cd21df13e711c27beeeb
Matthew Kubota & Rachael Miller <pair+mattk5.raemilla@devbootcamp.com>
2016-09-12 17:05:26 -0400

    add seed file and random listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 
   def show
     @category = Category.find(params[:category_id])
-    @article = Article.find(params[:id])
+    @article = @category.articles.find(params[:id])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = [
+  {name: "Mythical Creatures"},
+  {name: "Magic Potions"},
+  {name: "Spells"}
+]
+
+Category.create!(categories)
+
+articles = [
+  {name: "Unicorn", description: "A horse with a horn on its head -- V RARE!!", price:1000000, category_id: 1},
+  {name: "Leprechaun", description:"Generates unlimited gold!!!", price:2000000, category_id: 1},
+  {name: "Live Forever Elixir", description: "One drop adds 100 years to your life", price:4000000, category_id: 2},
+  {name: "Super Strength", description:"one cup gives you unparalleled strength for 1 hour", price: 1500000, category_id: 2},
+  {name: "Fairy Godmother Summoner", description: "Summons a fairy godmother for 24 hours. One use.", price: 3000000, category_id: 3}
+]
+
+Article.create!(articles)
